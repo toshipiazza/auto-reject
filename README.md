0f6e457e326fc3669dd72524312b63acf32c6194
Toshi Piazza <toshi.piazza@gmail.com>
2016-08-22 16:39:48 -0400

    Filters only non-System Header files
    - removes system header files from search
    - introduces hacky way to pull functions from clang via ctypes
      - TODO: mimick clang.ctypes python script, or ship own python
        script that supports the relevant methods

--- README.md
+++ README.md
@@ -3,7 +3,6 @@ Clang Plugin that rejects any instance of auto for C++11, for example in the cas
 Academic setting, where the use of auto might hinder learning.
 
 # Installation and Usage
-Requires clang and python-clang bindings. Note that because libclang cannot guess the
-proper include directories, it is necessary to manually apply them:
+Requires clang-3.9 and python-clang bindings.
 
-`$ ./auto.py --auto --rfor -isystem /usr/lib/clang/3.9/include/ test/main.cpp`
+`$ ./auto.py --auto --rfor test/main.cpp`
--- auto.py
+++ auto.py
@@ -1,28 +1,42 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
+from ctypes import cdll
 import clang.cindex
+import os
 
-# TODO: determine system headers
+# unfortunately we heavily depend on clang 3.9
+# FIXME: rely less heavily on user version
+GLOBAL_INCLUDE_DIRECTORY='/usr/lib/clang/3.9/include/'
+LD_LIBRARY_PATH='/usr/lib/x86_64-linux-gnu/'
+LD_LIBCLANG='libclang-3.9.so'
+
+# pull in the isInSystemHeader() function
+# FIXME: use format in clang.cindex, or otherwise hook into it in some form
+lib = cdll.LoadLibrary(LD_LIBRARY_PATH + LD_LIBCLANG)
+def clang_Location_isInSystemHeader(loc):
+    return lib.clang_Location_isInSystemHeader(loc)
 
 def get_diag_info(diag):
-    return "{} [line={}, col={}]".format(diag.spelling,
-           diag.location.line, diag.location.column)
+    return "{}:{}:{} {} ".format(diag.location.file.name,
+           diag.location.line, diag.location.column, diag.spelling)
 
 def visitor(node, auto, rfor):
     """ Find all auto-declared variables and range-based for loops
     """
     try:
-        if auto == True and node.kind.is_declaration() and \
-           node.type.kind == clang.cindex.TypeKind.AUTO:
-            # detect auto
-            print('Found auto declaration: {}:{}:{}'.format(
-                  node.location.file.name, node.location.line, node.location.column))
-        if rfor == True and node.kind.is_statement() and \
-           node.kind == clang.cindex.CursorKind.CXX_FOR_RANGE_STMT:
-            # detect ranged-based for loops
-            print('Found range-based for loop: {}:{}:{}'.format(
-                  node.location.file.name, node.location.line, node.location.column))
+        # ignore system headers...
+        if clang_Location_isInSystemHeader(node.location) == 0:
+            if auto == True and node.kind.is_declaration() and \
+               node.type.kind == clang.cindex.TypeKind.AUTO:
+                # detect auto
+                print('Found auto declaration: {}:{}:{}'.format(
+                      node.location.file.name, node.location.line, node.location.column))
+            if rfor == True and node.kind.is_statement() and \
+               node.kind == clang.cindex.CursorKind.CXX_FOR_RANGE_STMT:
+                # detect ranged-based for loops
+                print('Found range-based for loop: {}:{}:{}'.format(
+                      node.location.file.name, node.location.line, node.location.column))
     except ValueError:
         # unfortunately, the python bindings are incomplete and may throw ValueError
         # when we encounter a CursorKind which has not been explicitly written into the
@@ -44,8 +58,8 @@ if __name__=='__main__':
     (opts, args) = parser.parse_known_args()
 
     index = clang.cindex.Index.create()
-    # we allow c++11 by default
-    default_args = ['-isystem', '/usr/lib/clang/3.9/include/', '-std=c++11']
+    # we allow c++11 by default, and we spoof the system include directory.
+    default_args = ['-isystem', GLOBAL_INCLUDE_DIRECTORY, '-std=c++11']
     tu = index.parse(None, default_args + args)
     pprint(('diags', map(get_diag_info, tu.diagnostics)))
     if opts.auto or opts.rfor:
